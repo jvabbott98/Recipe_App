ef54527cdf65ad40b52f011b29d38cedc93d5c86
jvabbott98 <97220454+jvabbott98@users.noreply.github.com>
2024-09-26 01:08:55 -0400

    made migrations

--- src/recipes/models.py
+++ src/recipes/models.py
@@ -2,7 +2,7 @@ from django.db import models
 from ingredients.models import Ingredient
 
 class Recipe(models.Model):
-    name = models.CharField(max=120)
+    name = models.CharField(max_length=120)
 
     cooking_time = models.IntegerField(help_text='In minutes')
 
