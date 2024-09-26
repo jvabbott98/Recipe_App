ce87d78ee7e03c2448956973850aaed89002a1f2
jvabbott98 <97220454+jvabbott98@users.noreply.github.com>
2024-09-26 01:51:43 -0400

    ran tests

--- src/recipes/tests.py
+++ src/recipes/tests.py
@@ -1,3 +1,25 @@
 from django.test import TestCase
+from .models import Recipe
+from ingredients.models import Ingredient
 
-# Create your tests here.
+class RecipeModelTests(TestCase):
+    def test_create_recipe(self):
+        recipe = Recipe.objects.create(name="Tea", cooking_time = 5)
+        self.assertEqual(recipe.name, "Tea")
+        self.assertEqual(recipe.cooking_time, 5)
+
+    def test_retrieve_ingredients_from_recipe(self):
+        recipe = Recipe.objects.create(name="Tea", cooking_time = 5)
+        ingredient = Ingredient.objects.create(name="Water")
+
+        recipe.ingredients.add(ingredient)
+
+        retrieved_recipe = Recipe.objects.get(name = "Tea")
+        self.assertEqual(retrieved_recipe.ingredients.first(), ingredient)
+
+    def test_cooking_time_positive(self):
+        recipe = Recipe.objects.create(name="Tea", cooking_time = -10)
+        self.assertLess(recipe.cooking_time, 0)  # This should fail or raise an error
+
+
+
