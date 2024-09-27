6006e4ffecb9874699cc48f2b9029620890053c4
jvabbott98 <97220454+jvabbott98@users.noreply.github.com>
2024-09-27 01:02:00 -0400

    added author and cuisine models

--- src/recipes/models.py
+++ src/recipes/models.py
@@ -3,10 +3,30 @@ from ingredients.models import Ingredient
 
 class Recipe(models.Model):
     name = models.CharField(max_length=120)
+    author = models.CharField(max_length=120)
 
     cooking_time = models.IntegerField(help_text='In minutes')
 
+    description = models.TextField(default='No description...')
+    directions = models.TextField(default='No directions....')
+
     ingredients = models.ManyToManyField(Ingredient)
+
+    
+
+    
+
+    def calculate_difficulty(self):
+        if self.cooking_time < 10:
+            if len(self.ingredients) < 4:
+                self.difficulty = 'Easy'
+            else:
+                self.difficulty = 'Medium'
+        else:
+            if len(self.ingredients)< 4:
+                self.difficulty = 'Intermediate'
+            else:
+                self.difficulty = 'Hard'
 
     def __str__(self):
         return str(self.name)
