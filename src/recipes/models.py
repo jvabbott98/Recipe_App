b48619342a0b593989b05b5d1dbcb489d3a88397
jvabbott98 <97220454+jvabbott98@users.noreply.github.com>
2024-09-27 02:19:19 -0400

    task2-3 second attempt

--- src/recipes/models.py
+++ src/recipes/models.py
@@ -2,34 +2,46 @@ from django.db import models
 from ingredients.models import Ingredient
 from authors.models import Author
 from cuisines.models import Cuisine
+from django.db.models.signals import post_save
+from django.dispatch import receiver
 
 class Recipe(models.Model):
     name = models.CharField(max_length=120)
 
-    author = models.ForeignKey(Author, on_delete=models.CASCADE)
+    author = models.ForeignKey(Author, on_delete=models.SET_NULL, null=True, blank=True)
 
     cooking_time = models.IntegerField(help_text='In minutes')
 
     description = models.TextField(default='No description...')
     directions = models.TextField(default='No directions....')
 
-    ingredients = models.ManyToManyField(Ingredient)
-    cuisine = models.ManyToManyField(Cuisine)
-    
+    ingredients = models.ManyToManyField(Ingredient, related_name='ingredients')
+    cuisine = models.ManyToManyField(Cuisine, related_name='cuisine')
+
+
+    difficulty = models.CharField(max_length=20, blank=True)
 
     
+
 
     def calculate_difficulty(self):
+        ingredient_count = self.ingredients.count()
         if self.cooking_time < 10:
-            if len(self.ingredients) < 4:
-                self.difficulty = 'Easy'
+            if ingredient_count < 4:
+                return 'Easy'
             else:
-                self.difficulty = 'Medium'
+                return 'Medium'
         else:
-            if len(self.ingredients)< 4:
-                self.difficulty = 'Intermediate'
+            if ingredient_count < 4:
+                return 'Intermediate'
             else:
-                self.difficulty = 'Hard'
+                return 'Hard'
 
     def __str__(self):
         return str(self.name)
+
+@receiver(post_save, sender=Recipe)
+def set_difficulty(sender, instance, created, **kwargs):
+    if created:  
+        instance.difficulty = instance.calculate_difficulty()
+        instance.save(update_fields=['difficulty']) 
