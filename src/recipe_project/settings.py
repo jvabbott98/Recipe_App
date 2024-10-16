f5aaeccf05cc08b0b0528ce4f83d0bee1cb4d848
jvabbott98 <97220454+jvabbott98@users.noreply.github.com>
2024-10-16 00:29:37 -0400

    change debug to false

--- src/recipe_project/settings.py
+++ src/recipe_project/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY','django-insecure-ml49cp(e)=yakpe
 
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
