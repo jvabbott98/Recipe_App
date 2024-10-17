d7ffb9b17ea70bee6ef3e8ccd36a2a024885b407
jvabbott98 <97220454+jvabbott98@users.noreply.github.com>
2024-10-17 01:17:43 -0400

    Seperated data visualization from recipe_list view

--- src/recipes/forms.py
+++ src/recipes/forms.py
@@ -1,11 +1,13 @@
 from django import forms
 
 CHART__CHOICES = (        
-   ('#1', 'Bar chart'),   
-   ('#2', 'Pie chart'),
-   ('#3', 'Line chart')
+   ('#1', 'Number of ingredients'),   
+   ('#2', 'Difficulty'),
+   ('#3', 'Cooking Time')
    )
 
 class IngredientSearchForm(forms.Form):
     ingredient_name = forms.CharField(max_length=120)
+
+class ChartChoices(forms.Form):
     chart_type = forms.ChoiceField(choices=CHART__CHOICES)
--- src/recipes/urls.py
+++ src/recipes/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import home, recipe_list, recipe_detail
+from .views import home, recipe_list, recipe_detail, visualization
 
 app_name = 'recipes'
 
@@ -7,4 +7,5 @@ urlpatterns = [
     path('', home, name='home'),
     path('recipes/', recipe_list, name='recipes'),
     path('recipe/<int:id>/', recipe_detail, name='recipe_detail'),
+    path('recipes/visualization/', visualization, name='recipe_visualization'),
 ]
